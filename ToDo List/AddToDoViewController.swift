afb3e0be8e0772d940639220b9b1daede3244a0a
Raheem Bakare <raheem@raheems-macbook-pro.local>
2018-12-08 02:05:28 -0500

    added view controller

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by Raheem Bakare on 2018-12-08.
+//  Copyright © 2018 centennial college. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
