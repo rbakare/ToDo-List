3b4af999246066b5357e9a79dfffeeebcda5c3e2
Raheem Bakare <raheem@raheems-macbook-pro.local>
2018-12-08 01:53:28 -0500

    initial commit added Todo table view controller created a todo swift file

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Raheem Bakare on 2018-12-08.
+//  Copyright © 2018 centennial college. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var complete = false
+}
---
+++ ToDo List/ToDoTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ToDoTableViewController.swift
+//  ToDo List
+//
+//  Created by Raheem Bakare on 2018-12-08.
+//  Copyright © 2018 centennial college. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+  
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+   
+
+}
